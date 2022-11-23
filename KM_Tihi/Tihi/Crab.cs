a6d084ede3fb74659e515858f8d9c57cd440f03b
KenyerLord89 <113907107+KenyerLord89@users.noreply.github.com>
2022-11-23 15:39:14 +0100

    Add files via upload

---
+++ KM_Tihi/Tihi/Crab.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tihi
+{
+    internal class Crab :Food
+    {
+        int size;
+        public Crab(int size)
+        {
+            if (size >10||size<1)
+                Console.WriteLine("Ewww");
+            else
+                this.size = size;
+        }
+
+        public override int value()
+        {
+            return size*base.value();
+        }
+    }
+}
